54de4a181ac36da5471190b4927e83b234442e4b
A'Jahn Kimble <ajahnkimble@gmail.com>
2020-01-09 13:34:38 -0500

    Updates

---
+++ precache-manifest.5742b9cee3e6629f63a463b46c3132eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bbca50b35277aebe6f209e9a5f2731",
+    "url": "/Chat-App-React/index.html"
+  },
+  {
+    "revision": "aa70178bf10344c4d714",
+    "url": "/Chat-App-React/static/css/main.7365e6e7.chunk.css"
+  },
+  {
+    "revision": "2893b0dad1c75cf7bda0",
+    "url": "/Chat-App-React/static/js/2.f4b0e810.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Chat-App-React/static/js/2.f4b0e810.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa70178bf10344c4d714",
+    "url": "/Chat-App-React/static/js/main.7702bfd0.chunk.js"
+  },
+  {
+    "revision": "3a89428c5d6131176e37",
+    "url": "/Chat-App-React/static/js/runtime-main.46b8a14c.js"
+  },
+  {
+    "revision": "22141be9dacd40922500d6b52cee52ac",
+    "url": "/Chat-App-React/static/media/iconfinder_clip_115756.22141be9.svg"
+  },
+  {
+    "revision": "6f883f5cfe9ec9aee5565bfeda802b1d",
+    "url": "/Chat-App-React/static/media/search-svgrepo-com.6f883f5c.svg"
+  },
+  {
+    "revision": "f6b0cd46dc49799eb81d245e38dec191",
+    "url": "/Chat-App-React/static/media/trash-can-with-cover-from-side-view-svgrepo-com.f6b0cd46.svg"
+  }
+]);
